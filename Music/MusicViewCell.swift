d52777d8adf114ee1473e7d674b76b77a34c7e70
mcondemaita <mariacondemaita@gmail.com>
2021-11-21 12:40:36 -0500

    Updated API

---
+++ Music/MusicViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MusicViewCell.swift
+//  Music
+//
+//  Created by Maria  Condemaita on 11/21/21.
+//
+
+import UIKit
+
+class MusicViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
